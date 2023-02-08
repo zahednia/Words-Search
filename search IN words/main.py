a60c2daef72159fb384596faea1fdb3a3f4b7998
Creat1ve <70833007+Creat1vo@users.noreply.github.com>
2023-02-08 19:07:47 +0330

    Add files via upload

---
+++ search IN words/main.py
@@ -0,0 +1,13 @@
+# dar inja man file ro baz mikonim .
+file = open("main.txt")
+
+# dar inja file ro mikhonim .
+data = file.read()
+
+# Eslm baraye search ke az file txt bekhone .
+search_word = data.count("kourosh")
+
+# Run kardan Code va Print kardan  .
+print('Number of word :', search_word)
+
+# Kourosh Creat1ve
